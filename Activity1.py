5c84b48ab59b1cb59a5057f23d12c68388899cfe
Vivienne126 <vivienne.singh@cns.ac.in>
2026-01-06 18:23:07 +0530

    New

---
+++ Activity1.py
@@ -0,0 +1,13 @@
+a="Hello"
+b=5
+c=True
+d=14.5
+
+print(f"Type of a is {type(a)}")
+print(f"Type of b is {type(b)}")
+print(f"Type of c is {type(c)}")
+print(f"Type of d is {type(d)}")
+
+#Type Casting
+d=int(d)
+print(f"Type of d is {type(d)}")
