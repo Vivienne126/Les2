0d372bcee189e20c76dc25b3f6963243d3d4673b
Vivienne126 <vivienne.singh@cns.ac.in>
2026-01-06 18:52:46 +0530

    New4

---
+++ ACP1.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter number1"))
+num2=int(input("Enter number2"))
+num3=int(input("Enter number3"))
+d=num1
+num1=num2
+num2=num3
+num3=d
+print(f"The new value of num 1 is {num1}")
+print(f"The new value of num 2 is {num2}")
+print(f"The new value of num 3 is {num3}")
