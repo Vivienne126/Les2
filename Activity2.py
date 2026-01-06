86ffa07b5040fd2c3bfba486730d172dcdec2ddf
Vivienne126 <vivienne.singh@cns.ac.in>
2026-01-06 18:34:49 +0530

    New1

---
+++ Activity2.py
@@ -0,0 +1,18 @@
+a=50
+b=43
+
+print(f"The addition of a and b is: {a+b}")
+print(f"The subtraction of a and b is: {a-b}")
+print(f"The multiplication of a and b is: {a*b}")
+print(f"The division of a and b is: {a/b}")
+print(f"The modulus of a and b is: {a%b}")
+print(f"The floor division of a and b is: {a//b}")
+print(f"The square of a is: {a**2}")
+print(f"The square root of a is: {a**0.5}")
+
+
+#Conditional operators
+print(f"Are the numbers equal?? {a==b}")
+print(f"Are the numbers equal?? {a!=b}")
+print(f"Which number is greater or equal to {a>=b}")
+print(f"Whuch number is less or equal to {a<=b}")
