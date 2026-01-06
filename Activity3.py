0002207c192e61d9c5bcbffad332825c318e7fd4
Vivienne126 <vivienne.singh@cns.ac.in>
2026-01-06 18:39:51 +0530

    New3

---
+++ Activity3.py
@@ -0,0 +1,5 @@
+string="Hello"
+print(f"The length is , {len(string)}")
+print(f"The first character is , {string[0]}")
+print(f"The last character is , {string[4]}")
+print(f"The sliced string is , {string[0:2]}")
